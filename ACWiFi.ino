3f9d7788ec0243ccc71edf8b43faa4aa6cb2ffb8
Dan Visser <d.visser@vizztech.com.au>
2020-09-10 07:54:25 +1000

    Implemented OTA support.

--- ACWiFi.ino
+++ ACWiFi.ino
@@ -8,6 +8,7 @@
 #include "MHI-AC-Ctrl-core.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ArduinoOTA.h>
 
 MHI_AC_Ctrl_Core mhi_ac_ctrl_core;
 
@@ -168,7 +169,27 @@ void setup()
       WiFi.softAP(apssid); // enable AP mode
     }
   }
-  
+  Serial.println("Initializing OTA support.");
+  ArduinoOTA.setPassword("esp8266");
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA End");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");  
   Serial.println("Initializing DS18B20 temperature sensor.");
   setup_ds18x20();
 
@@ -212,7 +233,7 @@ void setup()
  
 void loop()
 {
-  
+  ArduinoOTA.handle();
 }
 
 void(* resetFunc) (void) = 0; // declare reset function at address 0, call resetFunc() to reset the system.
